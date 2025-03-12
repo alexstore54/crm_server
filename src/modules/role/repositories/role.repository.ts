93327560f7e9588f64bf768e4d1082dd4edaab7e
vishvishCode <olegvishnevskiy95@gmail.com>
2025-03-12 16:50:51 +0200

    implemented findRoleMethod

--- src/modules/role/repositories/role.repository.ts
+++ src/modules/role/repositories/role.repository.ts
@@ -1,25 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from '@/shared/db/prisma';
-import { getMockedRole } from '@/shared/mocks/role/role.mock';
 import { Role } from '@prisma/client';
+import { ERROR_MESSAGES } from '@/shared/constants/errors';
 
 @Injectable()
 export class RoleRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  public async findOneByPublicId(publicId: string): Promise<Role> {
-
-    //TODO: Implement this method
-
-    // try {
-    //   return this.prisma.role.findUnique({
-    //     where: {
-    //       publicId: publicId,
-    //     },
-    //   });
-    // } catch (error: any) {
-    //   throw new InternalServerErrorException(`${ERROR_MESSAGES.DB_ERROR}: ${error.message}`);
-    // }
-    return getMockedRole();
+  public async findOneByPublicId(publicId: string): Promise<Role | null> {
+    try {
+      return this.prisma.role.findUnique({
+        where: {
+          publicId: publicId,
+        },
+      });
+    } catch (error: any) {
+      throw new InternalServerErrorException(`${ERROR_MESSAGES.DB_ERROR}: ${error.message}`);
+    }
   }
 }
