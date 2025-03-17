924039adb49ee1f615cf56195ad96105e01f1dd2
Alex Store <storchiloalexander@gmail.com>
2025-03-17 18:10:49 +0200

    roles

--- src/modules/role/controllers/roles.controller.ts
+++ src/modules/role/controllers/roles.controller.ts
@@ -1,20 +1,34 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { RoleService } from '../services/role.service';
 import { ENDPOINTS } from '@/shared/constants/endpoints';
+import { UUIDValidationPipe } from '@/common/pipes';
 
 @Controller(ENDPOINTS.ROLE.BASE)
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
+  @Get(ENDPOINTS.ROLE.GET_ALL_ROLES)
+  async getRoles() {}
+
   @Get(ENDPOINTS.ROLE.GET_ONE)
-  async getRole() {}
+  async getRole(
+    @Param('publicId', UUIDValidationPipe) publicId: string,
+  ) {
+    return this.roleService.getRoleByPublicId(publicId);
+  }
 
   @Post(ENDPOINTS.ROLE.CREATE_ROLE)
   async createRole() {}
 
   @Patch(ENDPOINTS.ROLE.UPDATE_ONE_ROLE)
-  async updateRole() {}
+  async updateRole(
+    @Param('publicId', UUIDValidationPipe) publicId: string,
+  ) {
+
+  }
 
   @Delete(ENDPOINTS.ROLE.DELETE_ROLE)
-  async deleteRole() {}
+  async deleteRole(
+    @Param('publicId', UUIDValidationPipe) publicId: string,
+  ) {}
 }
