9f797d059912ff824774326d62aa4fc683f7374f
Blackjack_production <86078300+BlackjackDaniels54@users.noreply.github.com>
2025-03-17 13:36:01 +0200

    deleted dublicates

--- src/modules/role/role.module 2.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RoleRepository } from '@/modules/role/repositories/role.repository';
-
-@Module({
-  providers: [RoleRepository],
-  exports: [RoleRepository],
-})
-export class RoleModule { }
